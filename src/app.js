86766fd2e8746ef36ab5b323cdb1448dde6ec921
John Bowser <john.bow7720@gmail.com>
2020-08-13 14:47:42 -0600

    assignment completed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const { v4: uuid } = require('uuid')
+const API_TOKEN = process.env.API_TOKEN
+
+
 
 const app = express()
 
@@ -17,16 +20,18 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-// {
-//   id: UUID,
-//   firstName: String,
-//   lastName: String,
-//   address1: String,
-//   address2: String,
-//   city: String,
-//   state: String,
-//   zip: Number
-// }
+// validate auth Token in request, must match token in .env
+function bearerTokenValidation(req, res, next) {
+  const authHeader = req.get('Authorization')
+  const token = authHeader.split(' ')[1]
+
+  if(token !== API_TOKEN) {
+    return res
+      .status(401)
+      .send('Unauthorized request')
+  }
+  next()
+}
 
 const addressBook = [
   {
@@ -45,7 +50,7 @@ app.get('/address', (req, res) => {
   res.status(200).json(addressBook)
 })
 
-app.post('/address', (req, res) => {
+app.post('/address', bearerTokenValidation, (req, res) => {
   const { firstName, lastName, address1, address2, city, state, zip } = req.body
   
   // Do we have the info we need?
@@ -116,6 +121,17 @@ app.post('/address', (req, res) => {
     .status(201)
     .location(`http://localhost:8000/address/${id}`)
     .json(newAddress)
+})
+
+app.delete('/address/:id', bearerTokenValidation, (req, res) => {
+  const { id } = req.params
+  const addressIndex = addressBook.findIndex(item => item.id === id)
+
+  addressBook.splice(addressIndex, 1)
+
+  res
+    .status(204)
+    .end()
 })
 
 app.use(function errorHandler(error, req, res, next) {
